a0b25b4e653591f73b2591b45898545f7d355629
dominikvrbic <dominikvrbic01@gmail.com>
2021-07-04 22:11:06 +0200

    App: routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Center, Container } from '@chakra-ui/react';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
+import { HomePage } from './pages';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Center h="100vh">
+      <Container h="100vh" maxW="8xl" centerContent className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/hero/:id" component={HomePage} />
+            <Route>
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </Router>
+      </Container>
+    </Center>
+  );
 }
 
 export default App;
