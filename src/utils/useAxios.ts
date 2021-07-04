32c5cff615407f6f318d84e7c1bc69bea02df136
dominikvrbic <dominikvrbic01@gmail.com>
2021-07-04 22:26:05 +0200

    utils/useAxios: create

---
+++ src/utils/useAxios.ts
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+
+import { AxiosContext } from './AxiosContext';
+/**
+ *
+ * @returns returns the axios instance with auth
+ */
+export const useAxios = () => useContext(AxiosContext);
