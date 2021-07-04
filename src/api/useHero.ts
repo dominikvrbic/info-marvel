e4892fb784ebe102394c3b11804c2de34e9dcd97
dominikvrbic <dominikvrbic01@gmail.com>
2021-07-05 01:27:47 +0200

    basic get data done

--- src/api/useHero.ts
+++ src/api/useHero.ts
@@ -1,9 +1,12 @@
 import { useAxios } from '../utils/useAxios';
+import { useQuery, UseQueryResult } from 'react-query';
 
 export function useHero(id: string) {
   const axios = useAxios();
 
-  //   const getHero =()=>{
-  //       const
-  //   }
+  const getHero = async () => {
+    const { data } = await axios.get(`characters/${id}/comics`);
+    return data;
+  };
+  return useQuery(['hero', id], getHero);
 }
--- src/pages/Hero.page.tsx
+++ src/pages/Hero.page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Redirect, useParams } from 'react-router';
+import { useHero } from '../api';
 
 interface Props {}
 
 export const Hero = (props: Props) => {
-  return <div></div>;
+  const { id } = useParams<{ id: string }>();
+  if (!id) return <Redirect to="/" />;
+  useHero(id);
+  return <div>Hero</div>;
 };
