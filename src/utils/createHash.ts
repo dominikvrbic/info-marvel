78f284866f84f74af911b7a64537ebd48dd90b7c
dominikvrbic <dominikvrbic01@gmail.com>
2021-07-04 22:26:31 +0200

    utils/createHash: update

--- src/utils/createHash.ts
+++ src/utils/createHash.ts
@@ -1,4 +1,9 @@
 import md from 'md5';
+
+/**
+ *
+ * @returns all of the data that is needed to be added to the api calls for 'auth'
+ */
 export function createHash(): {
   timestamp: string;
   publicKey: string;
