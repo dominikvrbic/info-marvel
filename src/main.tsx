2c8b50f6b84948a1a80b2bcc08fab6c7a7931ccf
dominikvrbic <dominikvrbic01@gmail.com>
2021-07-04 23:06:52 +0200

    main: add providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { AxiosProvider } from './utils';
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <AxiosProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </AxiosProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
