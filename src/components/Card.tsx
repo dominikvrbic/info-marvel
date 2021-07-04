84d62d84d325d358850f8465a4b91c7b2be07cf1
dominikvrbic <dominikvrbic01@gmail.com>
2021-07-05 00:08:46 +0200

    components/Card: format card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Box, BoxProps, Flex, Image, Text } from '@chakra-ui/react';
+import {
+  AspectRatio,
+  Box,
+  BoxProps,
+  Flex,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 
 interface Props {
   title: string;
-  subtitle?: string;
+  comicsCount?: number;
   imageUrl?: string;
 }
 
@@ -13,20 +20,23 @@ const MotionBox = motion<BoxProps>(Box);
 export const Card = ({
   imageUrl = 'https://via.placeholder.com/400',
   title,
-  subtitle,
+  comicsCount = 0,
 }: Props) => {
   return (
     <MotionBox
+      h="22rem"
       bg="#303030"
-      maxW="200px"
-      whileHover={{ scale: 1.1 }}
+      maxW={['full', '200px']}
+      whileHover={{ scale: 1.02 }}
       whileTap={{ scale: 0.9 }}
       sx={{
         clipPath: 'polygon(0 0, 100% 0, 100% 80%, 85% 100%, 0 100%)',
         WebkitClipPath: 'polygon(0 0, 100% 0, 100% 90%, 85% 100%, 0 100%)',
       }}
     >
-      <Image src={imageUrl} w="full" />
+      <AspectRatio h="10rem" w="full" ratio={4 / 3}>
+        <Image src={imageUrl} w="full" h="full" />
+      </AspectRatio>
       <Box bg="red" w="full" h="7px"></Box>
       <Flex
         direction="column"
@@ -37,7 +47,7 @@ export const Card = ({
         p={4}
       >
         <Text>{title}</Text>
-        {subtitle && <Text>{subtitle}</Text>}
+        <Text>{comicsCount} Comics</Text>
       </Flex>
     </MotionBox>
   );
